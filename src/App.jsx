e5af690e0974a19db7191570bab5f6f9527de3cc
shaheerali838 <shaheer838838@gmail.com>
2025-04-13 00:52:41 +0500

    axios added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import axios from "axios";
 
 const App = () => {
-  return <div>App</div>;
+  let key = "837452185e3c4c64874c17d6aea6bba5";
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=Peshawar&appid=${key}`;
+  let weatherCall = () => {
+    axios
+      .get("/user?ID=12345")
+      .then(function (response) {
+        // handle success
+        console.log(response);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      });
+  };
+  return (
+    <div>
+      <button onClick={weatherCall}>Click me</button>
+    </div>
+  );
 };
-
-export default App;
