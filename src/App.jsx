0d8f586a2b639f917337c60db9c10bd690e95476
simpwae <naqashamjad777@gmail.com>
2025-04-13 00:47:12 +0500

    axios integrate

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import axios from 'axios'
 
 const App = () => {
+  let key ='837452185e3c4c64874c17d6aea6bba5'
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=Peshawar&appid=${key}`
+  let weatherCall =()=>{
+    axios.get('/user?ID=12345')
+    .then(function (response) {
+      // handle success
+      console.log(response.);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+  }
   return (
-    <div>App</div>
+    <div><button onClick={weatherCall}>Click me</button></div>
   )
 }
 
