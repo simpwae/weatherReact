2af79148c3ff0679cdd70bc2c9073213d98bf112
shaheerali838 <shaheer838838@gmail.com>
2025-04-14 00:20:42 +0500

    done all done

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,7 +1,24 @@
 import axios from "axios";
 import React, { useState } from "react";
+import img from "../../public/img.png";
 
 const Layout = () => {
+  const cloudy = "/cloudy.png";
+  const rain = "/lightRain.png";
+  const sunny = "/sunny.png";
+
+  const imagesSrc = (temp) => {
+    if (temp > 30) {
+      return sunny;
+    } else if (temp <= 30) {
+      return rain;
+    } else {
+      return cloudy;
+    }
+  };
+
+  const [inputCity, setInputCity] = useState(""); // User input
+
   const [city, setCity] = useState();
   const [feelLike, setFeelLike] = useState();
   const [humidity, setHumidity] = useState();
@@ -9,14 +26,18 @@ const Layout = () => {
   const [maxTemp, setMaxTemp] = useState();
   const [minTemp, setMinTemp] = useState();
   const [windSpeed, setWindSpeed] = useState();
-  let weatherData = () => {
-    let key = "837452185e3c4c64874c17d6aea6bba5";
-    let city = "Peshawar";
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`;
+  const [weather, setWeather] = useState("second");
+  const [weatherIcon, setWeatherIcon] = useState();
+  let weatherIconImgUrl = `https://openweathermap.org/img/wn/${weatherIcon}@2x.png`;
+  const weatherData = () => {
+    const key = "837452185e3c4c64874c17d6aea6bba5";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputCity}&appid=${key}&units=metric`;
+
     axios
       .get(url)
-      .then(function (response) {
-        // handle success
+      .then((response) => {
+        setWeather(response.data.weather[0].main);
+        setWeatherIcon(response.data.weather[0].icon);
         setFeelLike(response.data.main.feels_like);
         setHumidity(response.data.main.humidity);
         setTemp(response.data.main.temp);
@@ -24,24 +45,33 @@ const Layout = () => {
         setMinTemp(response.data.main.temp_min);
         setCity(response.data.name);
         setWindSpeed(response.data.wind.speed);
-
         console.log(response);
       })
-      .catch(function (error) {
-        // handle error
+      .catch((error) => {
         console.log(error);
+        alert("City not found!");
       });
   };
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      weatherData();
+    }
+  };
+
   return (
     <section className="p-3 text-center text-gray-100 flex justify-center font-medium overflow-y-hidden">
       <div className="w-72 rounded-xl h-[96vh] bg-gray-500 p-3">
         <input
           type="text"
-          className="rounded-xl flex text-gray-800 justify-center mx-auto bg-gray-200 w-52 px-4 outline-none h-7 my-5"
-          onBlur={weatherData}
+          placeholder="Enter city name"
+          className="rounded-xl text-center text-gray-800 bg-gray-200 w-52 px-4 outline-none h-7 my-5"
+          value={inputCity}
+          onChange={(e) => setInputCity(e.target.value)}
+          onKeyDown={handleKeyDown}
         />
         <img
-          src="/img.png"
+          src={!weatherIcon ? img : weatherIconImgUrl}
           className="w-36 mx-auto object-cover"
           alt="Weather IMG"
         />
@@ -51,7 +81,7 @@ const Layout = () => {
         </h2>
         <div className="flex justify-between p-5">
           <p>{city}</p>
-          <p>Day</p>
+          <p>{weather}</p>
         </div>
         <hr />
         <div className="my-4">
@@ -72,7 +102,7 @@ const Layout = () => {
               <p className="font-semibold text-1xl">Humidity</p>
             </div>
           </div>
-          <div className="">
+          <div>
             <h4 className="font-semibold text-2xl">{windSpeed}km/h</h4>
             <p className="font-semibold text-1xl">Wind Speed</p>
           </div>
