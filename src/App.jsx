19c4519ae90597b7b4756ed7feb64c8080acedfc
shaheerali838 <shaheer838838@gmail.com>
2025-04-13 23:12:42 +0500

    OP

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,8 @@ import axios from "axios";
 import Layout from "./components/Layout";
 
 const App = () => {
-  let key = "837452185e3c4c64874c17d6aea6bba5";
-  let url = `https://api.openweathermap.org/data/2.5/weather?q=Peshawar&appid=${key}`;
-  let weatherCall = () => {
-    axios
-      .get("/user?ID=12345")
-      .then(function (response) {
-        // handle success
-        console.log(response);
-      })
-      .catch(function (error) {
-        // handle error
-        console.log(error);
-      });
-  };
   return (
     <main className="overflow-y-hidden h-screen bg-gray-800">
-      {/* <WeatherCard /> */}
       <Layout />
     </main>
   );
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,40 +1,85 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 
 const Layout = () => {
+  const [city, setCity] = useState();
+  const [feelLike, setFeelLike] = useState();
+  const [groundLevel, setGroundLevel] = useState();
+  const [humidity, setHumidity] = useState();
+  const [pressure, setPressure] = useState();
+  const [seaLevel, setSeaLevel] = useState();
+  const [temp, setTemp] = useState();
+  const [maxTemp, setMaxTemp] = useState();
+  const [minTemp, setMinTemp] = useState();
+  const [windSpeed, setWindSpeed] = useState();
+  let weatherData = () => {
+    let key = "837452185e3c4c64874c17d6aea6bba5";
+    let city = "Peshawar";
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`;
+    axios
+      .get(url)
+      .then(function (response) {
+        // handle success
+        setFeelLike(response.data.main.feels_like);
+        setGroundLevel(response.data.main.grnd_like);
+        setHumidity(response.data.main.humidity);
+        setPressure(response.data.main.pressure);
+        setSeaLevel(response.data.main.sea_level);
+        setTemp(response.data.main.temp);
+        setMaxTemp(response.data.main.temp_max);
+        setMinTemp(response.data.main.temp_min);
+        setCity(response.data.name);
+        setWindSpeed(response.data.wind.speed);
+
+        console.log(response);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      });
+  };
   return (
     <section className="p-3 text-center text-gray-100 flex justify-center font-medium overflow-y-hidden">
-      <div className="w-72 rounded-xl h-auto bg-gray-500 p-3">
+      <div className="w-72 rounded-xl h-[96vh] bg-gray-500 p-3">
         <input
           type="text"
           className="rounded-xl flex text-gray-800 justify-center mx-auto bg-gray-200 w-52 px-4 outline-none h-7 my-5"
+          onBlur={weatherData}
         />
         <img
           src="/img.png"
-          className="w-72 h-52 object-cover"
+          className="w-36 mx-auto object-cover"
           alt="Weather IMG"
         />
         <h2 className="text-5xl font-semibold">
-          30<sup>o</sup>C
+          {temp}
+          <sup>o</sup>C
         </h2>
         <div className="flex justify-between p-5">
-          <p>Location</p>
+          <p>{city}</p>
           <p>Day</p>
         </div>
         <hr />
         <div className="my-4">
-          <p>Light Rain</p>
-          <p>Min-temperature</p>
-          <p>Max-temperature</p>
+          <p className="flex justify-between">
+            <span>Feels Like:</span> {feelLike}
+          </p>
+          <p className="flex justify-between">
+            <span>Min-Temp:</span> {minTemp}
+          </p>
+          <p className="flex justify-between">
+            <span>Max-Temp:</span> {maxTemp}
+          </p>
         </div>
         <div className="bg-gray-700 p-5 flex rounded-lg justify-between">
           <div>
             <div>
-              <h4 className="font-semibold text-2xl">83%</h4>
+              <h4 className="font-semibold text-2xl">{humidity}%</h4>
               <p className="font-semibold text-1xl">Humidity</p>
             </div>
           </div>
           <div className="">
-            <h4 className="font-semibold text-2xl">6km/h</h4>
+            <h4 className="font-semibold text-2xl">{windSpeed}km/h</h4>
             <p className="font-semibold text-1xl">Wind Speed</p>
           </div>
         </div>
